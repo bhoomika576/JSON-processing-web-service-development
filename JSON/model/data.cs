40d0017eea3ad35cc375904b5c868a1546a393cc
bhoomika576 <bhoomikamudhoo@gmail.com>
2026-01-05 17:56:13 +0400

    Add project files.

---
+++ JSON/model/data.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+    namespace JSON.model
+    {
+        public class data
+        {
+
+            [JsonPropertyName("Vehicle_ID")]
+            public int VehicleID { get; set; }
+
+            [JsonPropertyName("Category")]
+            public string Category { get; set; }
+
+            [JsonPropertyName("Reason")]
+            public string Reason { get; set; }
+
+            [JsonPropertyName("Number_of_Incidents")]
+            public int Num_Of_Incidents { get; set; }
+
+            [JsonPropertyName("Operable_Region")]
+            public int Region { get; set; }
+
+            [JsonPropertyName("last_inspection")]
+            public DateOnly LastInspection { get; set; }
+
+            [JsonPropertyName("Inspector_ID")]
+            public int InspectorID { get; set; }
+        }
+
+
+        public class VehicleData
+        {
+            [JsonPropertyName("Vehicles")]
+
+            public List<data> vehicles { get; set; }
+        }
+    }
