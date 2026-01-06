e9220bd167fe239031b21ef11fc62383f8a9fdd4
Raxxit <raxxit004@gmail.com>
2026-01-06 14:17:24 +0400

    Reapply "completed first part assignment exercise 2"
    This reverts commit 743883afa4519c1364cda69c589337972d89432e.

--- JSON/Program.cs
+++ JSON/Program.cs
@@ -1,6 +1,4 @@
-﻿using JSON;
-using JSON.model;
-using System.Reflection.PortableExecutable;
+﻿using JSON.model;
 
 namespace JSON
 {
@@ -18,6 +16,33 @@ namespace JSON
             //{
             //    Console.WriteLine($"ID: {v.VehicleID} | Date: {v.LastInspection}");
             //}
+            
+            reader reader = new reader();
+            List<Vehicle> myVehicles = reader.ReadJson();
+
+            foreach (var v in myVehicles)
+            {
+                Console.WriteLine(new string('=', 50));
+
+                // Header: [ID] Name
+                Console.WriteLine($"[{v.VehicleID}] {v.Category}");
+                Console.WriteLine(new string('-', 50));
+
+                // reason and number of incidents
+                Console.WriteLine($"\n--- Risks ---");
+
+                Console.WriteLine($"Reason why vehicle is deemed risky:   {v.Reason}");
+                Console.WriteLine($"Number of Incidents: {v.NumOfIncidents}");
+
+                // region operable 
+                Console.WriteLine($"\n---Region Operable ---");
+                Console.WriteLine($"Region Number: {v.OperableRegion}");
+
+                // Footer
+                Console.WriteLine(new string('-', 50));
+                Console.WriteLine($"Last Inspected: {v.LastInspection}");
+                Console.WriteLine("\n");
+            }
 
 
         }
