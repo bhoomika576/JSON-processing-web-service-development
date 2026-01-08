daf3817383e0a5a35be76972e34cb866e00da73a
zendrax26 <jugnarain1234@gmail.com>
2026-01-08 17:47:02 +0400

    sorting class done

---
+++ JSON/sorting.cs
@@ -0,0 +1,18 @@
+﻿using JSON.model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JSON //.model
+{
+    public static class VehicleSorter
+    {
+        public static List<Vehicle> SortVehicles(List<Vehicle> vehicles)
+    {
+        return vehicles.OrderBy(v => v.Category)
+            .ThenBy(v => v.OperableRegion)
+            .ToList();
+   
+    
+        }
+    }
+}
